6414720ffb979877a006f59d8304804032a93640
RafaelAlison <RafaelAlison@192.168.0.106>
2014-10-27 16:03:53 -0300

    Commit inicial

---
+++ SalaDeBatePapo/src/Sala/ServidorBatePapo.java
@@ -0,0 +1,104 @@
+package Sala;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class ServidorBatePapo extends Thread {
+    
+	private static Map<String, PrintStream> MAP_Usuarios;
+    private Socket conexao;
+    private String usuario;
+    private static List<String> Nomes_Usuarios = new ArrayList<String>();
+    
+    public ServidorBatePapo(Socket socket) {
+        this.conexao = socket;
+    }
+    public boolean armazenaNome(String novoUsuario) {
+        for (int i = 0; i < Nomes_Usuarios.size(); i++) {
+            if (Nomes_Usuarios.get(i).equals(novoUsuario))
+                return true;
+        }
+        Nomes_Usuarios.add(novoUsuario);
+        return false;
+    }
+    public void remove(String exUsuario) {
+        for (int i = 0; i < Nomes_Usuarios.size(); i++) {
+            if (Nomes_Usuarios.get(i).equals(exUsuario))
+            	Nomes_Usuarios.remove(exUsuario);
+        }
+    }
+    public static void main(String args[]) {
+        MAP_Usuarios = new HashMap<String, PrintStream>();
+        try {
+            ServerSocket server = new ServerSocket(5555);
+            System.out.println("ServidorSocket rodando na porta 5555");
+            while (true) {
+                Socket conexao = server.accept();
+                Thread t = new ServidorBatePapo(conexao);
+                t.start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+    public void run() {
+        try {
+            BufferedReader entrada =
+                new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+            this.usuario = entrada.readLine();
+            if (armazenaNome(this.usuario)) {
+                saida.println("Este nome ja existe! Conecte novamente com outro Nome.");
+                this.conexao.close();
+                return;
+            } else {
+                //Mostra o nome do usuario conectado ao servidor;
+                System.out.println(this.usuario + " : Conectado ao Servidor!");
+                //Quando o usuario se conectar recebe todos que estão conectados;
+                saida.println("Conectados: " + Nomes_Usuarios.toString());
+            }
+            if (this.usuario == null) {
+                return;
+            }
+            //Adiciona os dados de saida do ususario no objeto MAP_Usuarios
+            //A chave será o nome e valor o printstream;
+            MAP_Usuarios.put(this.usuario, saida);
+            String[] msg = entrada.readLine().split(":");
+            while (msg != null && !(msg[0].trim().equals(""))) {
+                send(saida, " escreveu: ", msg);
+                msg = entrada.readLine().split(":");
+            }
+            System.out.println(this.usuario + " saiu do bate-papo!");
+            String[] out = {" do bate-papo!"};
+            send(saida, " saiu", out);
+            remove(this.usuario);
+            MAP_Usuarios.remove(this.usuario);
+            this.conexao.close();
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+    
+    public void send(PrintStream saida, String acao, String[] msg) {
+        out:
+        for (Map.Entry<String, PrintStream> cliente : MAP_Usuarios.entrySet()) {
+            PrintStream chat = cliente.getValue();
+            if (chat != saida) {
+                if (msg.length == 1) {
+                    chat.println(this.usuario + acao + msg[0]);
+                } else {
+                    if (msg[1].equalsIgnoreCase(cliente.getKey())) {
+                        chat.println(this.usuario + acao + msg[0]);
+                        break out;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ SalaDeBatePapo/src/Sala/UsuarioBatePapo.java
@@ -0,0 +1,80 @@
+package Sala;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class UsuarioBatePapo extends Thread {
+    
+	// Controla a recepção de mensagens do usuario
+    private Socket conexao;
+    // Construtor que recebe o socket do usuario
+    public UsuarioBatePapo(Socket socket) {
+        this.conexao = socket;
+    }
+    
+    public static void main(String args[])
+    {
+        try {
+            //Instancia do atributo conexao do tipo Socket;
+            //Conecta o IP do Servidor e a Porta;
+            Socket socket = new Socket("127.0.0.1", 5555);
+            //Instancia do atributo saida, obtem os objetos que permitem;
+            //Controlar o fluxo de comunicação;
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Digite seu nome de usuário: ");
+            String meuNome = teclado.readLine();
+            //Envia o nome digitado para o servidor;
+            saida.println(meuNome.toUpperCase());
+            //Instancia a thread para ip e porta conectados e depois inicia ela;
+            Thread thread = new UsuarioBatePapo(socket);
+            thread.start();
+            //Cria a variavel msg responsavel por enviar a mensagem para o servidor;
+            String msg;
+            while (true)
+            {
+                //Cria linha para digitação da mensagem e a armazena na variavel msg;
+                System.out.print("Mensagem > ");
+                msg = teclado.readLine();
+                //Envia a mensagem para o servidor;
+                saida.println(msg);
+            }
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+    //Execução da thread;
+    public void run()
+    {
+        try {
+            //Recebe mensagens de outro cliente através do servidor;
+            BufferedReader entrada =
+                new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            //Cria variavel de mensagem;
+            String msg;
+            while (true)
+            {
+                //Pega o que o servidor enviou;
+                msg = entrada.readLine();
+                //Se a mensagem contiver dados, passa pelo if;
+                //Caso contrario cai no break e encerra a conexao;
+                if (msg == null) {
+                    System.out.println("Conexão encerrada!");
+                    System.exit(0);
+                }
+                System.out.println();
+                //Imprime a mensagem recebida;
+                System.out.println(msg);
+                //Cria uma linha visual para resposta;
+                System.out.print("Responder > ");
+            }
+        } catch (IOException e) {
+            //Caso ocorra alguma exceção de E/S, mostra qual foi;
+            System.out.println("Ocorreu uma Falha... .. ." +
+                " IOException: " + e);
+        }
+    }
+}
